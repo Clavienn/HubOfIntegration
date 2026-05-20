704941c9691a43ac5d4a4cd9c24f5bd932a259e0
Clavienn <claviennambinina@gmail.com>
2026-05-21 00:56:54 +0300

    corrge api

--- api/src/routes/ingest.routes.ts
+++ api/src/routes/ingest.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { IngestController } from '../controllers/ingest.controller';
-import { validateIngestRequest } from '../middlewares/validation.middleware';
+// import { validateIngestRequest } from '../middlewares/validation.middleware';
 
 const router = Router();
 const ingestController = new IngestController();
 
-router.post('/', validateIngestRequest, ingestController.ingest.bind(ingestController));
+router.post('/', ingestController.ingest.bind(ingestController));
 router.get('/:id/status', ingestController.getMessageStatus.bind(ingestController));
 router.post('/:id/replay', ingestController.replayMessage.bind(ingestController));
 
